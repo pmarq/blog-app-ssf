4d3c741e7f1929a26af0b1a7ef1e6cb1ee98bd61
Paulo Marques <paulomuniz2411@gmail.com>
2025-01-14 18:55:43 -0300

    features 14012025

--- app/components/layout/DefaultLayout.tsx
+++ app/components/layout/DefaultLayout.tsx
@@ -21,7 +21,7 @@ const DefaultLayout: React.FC<DefaultLayoutProps> = ({
         <meta name="description" content={desc} />
         {/* Adicione outros metadados e links aqui, se necessário */}
       </Head>
-      <div className="max-w-4xl mx-auto p-4">{children}</div>
+      <div className="max-w-8xl mx-auto p-4">{children}</div>
     </div>
   );
 };
