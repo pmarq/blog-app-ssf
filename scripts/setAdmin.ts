0d396a0c1ebf66818e86894b65220ee2434e0897
Paulo Marques <paulomuniz2411@gmail.com>
2025-05-20 17:28:01 -0300

    firebase setup admin

---
+++ scripts/setAdmin.ts
@@ -0,0 +1,25 @@
+// scripts/setAdmin.ts
+// scripts/setAdmin.js
+
+const { admin } = require("../firebase/server");
+
+async function setAdminClaims() {
+  const emailsString = process.env.ADMIN_EMAILS;
+  if (!emailsString) {
+    throw new Error("ADMIN_EMAILS não está definido no .env");
+  }
+
+  const emails = emailsString.split(",").map((e) => e.trim());
+
+  for (const email of emails) {
+    try {
+      const user = await admin.auth().getUserByEmail(email);
+      await admin.auth().setCustomUserClaims(user.uid, { admin: true });
+      console.log(`✅ Admin claim aplicada para: ${email}`);
+    } catch (error) {
+      console.error(`❌ Erro ao aplicar claim para ${email}:`, error);
+    }
+  }
+}
+
+setAdminClaims();
