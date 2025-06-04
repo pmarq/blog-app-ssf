9a9cc4a6a515d881031d943412b9443f72e208af
pmarq <paulomuniz2411@gmail.com>
2025-06-04 20:48:17 -0300

    render bug 0406

--- app/(admin)/dashboard/posts/page.tsx
+++ app/(admin)/dashboard/posts/page.tsx
@@ -1,11 +1,10 @@
 // app/(admin)/dashboard/posts/page.tsx
-
+export const dynamic = "force-dynamic";
 
 import React from "react";
 import AdminLayout from "@/app/components/layout/AdminLayout";
 import PostsListWrapper from "@/app/components/common/PostListWrapper";
 import { fetchInitialPosts } from "@/lib/fetchPosts";
-
 
 export default async function Posts() {
   const limit = 9;
