f8b5fa54ae5f4a572f20d9d1a1f1e856e4f613a5
pmarq <paulomuniz2411@gmail.com>
2025-06-09 19:57:45 -0300

    layout css 2 0606

--- app/components/common/InfiniteScrollPosts.tsx
+++ app/components/common/InfiniteScrollPosts.tsx
@@ -37,7 +37,7 @@ const InfiniteScrollPosts: FC<Props> = ({
       loader={loader || defaultLoader}
     >
       <div className="max-w-8xl mx-auto p-3">
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {posts.map((post) => (
             <PostCard
               key={post.slug}
