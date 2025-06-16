420d87e47942da87e205ae5d169e37607ad45e13
pmarq <paulomuniz2411@gmail.com>
2025-06-16 20:00:21 -0300

    user auth 1606

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,45 +1,25 @@
-// firebase/client.ts
-
+// /firebase/client.ts
 import { initializeApp, getApps } from "firebase/app";
-import { Auth, getAuth } from "firebase/auth";
-import { FirebaseStorage, getStorage } from "firebase/storage";
-import { Firestore, getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { coreConfig } from "../lib/firebase/coreConfig"; // Auth centralizado
+import { blogConfig } from "@/lib/firebase/blogConfig";
+// Firestore do blog
 
-// Seu arquivo de configuração do Firebase
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+let coreApp, blogApp;
 
-// Logs para depuração
-console.log("Firebase Config:", firebaseConfig);
-console.log("Apps (antes de inicializar):", getApps());
-
-// Verifica se já existe uma instância do app
-const currentApps = getApps();
-
-let auth: Auth;
-let storage: FirebaseStorage;
-let db: Firestore;
-
-if (!currentApps.length) {
-  const app = initializeApp(firebaseConfig);
-  auth = getAuth(app);
-  storage = getStorage(app);
-  db = getFirestore(app);
+// Auth centralizado
+if (!getApps().some((app) => app.name === "core")) {
+  coreApp = initializeApp(coreConfig, "core");
 } else {
-  const app = currentApps[0];
-  auth = getAuth(app);
-  storage = getStorage(app);
-  db = getFirestore(app);
+  coreApp = getApps().find((app) => app.name === "core")!;
 }
+export const auth = getAuth(coreApp);
 
-// Logs finais
-console.log("Apps (depois de inicializar):", getApps());
-
-// Exporta tudo o que você precisar usar no client
-export { auth, storage, db };
+// Firestore do blog
+if (!getApps().some((app) => app.name === "blog")) {
+  blogApp = initializeApp(blogConfig, "blog");
+} else {
+  blogApp = getApps().find((app) => app.name === "blog")!;
+}
+export const db = getFirestore(blogApp);
