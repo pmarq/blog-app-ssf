4a4cb8f793badb9b55d5e3229c3e9b0880f24713
pmarq <paulomuniz2411@gmail.com>
2025-06-30 22:57:54 -0300

    create user blog VI 3006

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   basePath: isProd ? "/blog" : "",
+  trustHostHeader: true,
   trailingSlash: false,
   images: {
     remotePatterns: [
